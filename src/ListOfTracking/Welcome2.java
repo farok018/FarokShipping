052bb0d14413bbdec00f4c9149d6cc9950a6b339
farok <farok@10.0.0.11>
2023-02-13 21:46:20 -0600

    Check the people list

---
+++ src/ListOfTracking/Welcome2.java
@@ -0,0 +1,13 @@
+package ListOfTracking;
+
+public class Welcome2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("starting new again");
+		System.out.println(2+5);
+		System.out.println(51*11);
+		System.out.println("By Farok");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author farok
+ *
+ */
+module FarokShipping {
+}
