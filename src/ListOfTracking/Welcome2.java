a3f54fe0cdf29b5e2495bcac8282b888a6abee5c
farok <farok@10.0.0.11>
2023-02-19 22:20:56 -0600

    test again

--- src/ListOfTracking/Welcome2.java
+++ src/ListOfTracking/Welcome2.java
@@ -23,6 +23,7 @@ public class Welcome2 {
 		System.out.println("check new one");
 		System.out.println("what is the prob");
 		System.out.println("hello test");
+		System.out.println("what happening");
 	}
 
 }
